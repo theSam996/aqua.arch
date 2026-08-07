990d7916e306326c5d0a0167ac0241942bd1c751
Samar Kumar <rishukumarici@gmail.com>
2026-08-07 15:38:38 +0530

    redo

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors({
     origin: [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "https://aqua-arch.vercel.app"
+    "https://your-app.vercel.app"
   ],
   credentials: true
 }));
