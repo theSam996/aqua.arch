d29aea8c6a149c55b6406b03c6f38c9c575983db
Samar Kumar <rishukumarici@gmail.com>
2026-08-07 15:37:26 +0530

    fronted in backend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors({
     origin: [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "https://your-app.vercel.app"
+    "https://aqua-arch.vercel.app"
   ],
   credentials: true
 }));
